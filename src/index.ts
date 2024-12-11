6aa8f722fdc11a9cb61134c6d4ed9723177ac3d4
Martin Chirazi <dkmarm@chr-hansen.com>
2024-12-11 14:24:13 +0100

    optimizing for export

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as HelpDesk } from "./App";
+export { useHelpDeskContext } from "./App";
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths()],
 	css: {
 		preprocessorOptions: {
 			scss: {
@@ -15,17 +15,26 @@ export default defineConfig({
 	},
 	build: {
 		lib: {
-			entry: path.resolve("src/index.tsx"),
+			//Defines the entry point for the library build. It resolves
+			//to src/index.ts,indicating that the library starts from this file.
+			entry: path.resolve("src/index.ts"),
 			name: "react-help-desk-tailwindcss-ts",
-			fileName: (format) => `react-help-desk-tailwindcss-ts.${format}.ts`,
+			//A function that generates the output file
+			//name for different formats during the build
+			fileName: (format) => `index.${format}.js`,
 		},
 		rollupOptions: {
-			external: ["react", "react-dom"],
+			external: ["react", "react-dom", "react/jsx-runtime"],
 			output: {
 				globals: {
 					react: "React",
+					"react-dom": "ReactDOM",
+					"react/jsx-runtime": "react/jsx-runtime",
 				},
 			},
 		},
+		sourcemap: true,
+		emptyOutDir: true,
 	},
+	plugins: [react(), viteTsconfigPaths(), dts()],
 });
